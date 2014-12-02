e809283669c8ed0bbc9541f1afdb1e599838c311
chrisbrasington <chrisbrasington@gmail.com>
2014-12-01 20:11:01 -0700

    dungeon 1 refinement

--- dungeon1.hs
+++ dungeon1.hs
@@ -1,15 +1,15 @@
 import Euterpea
 
 mel1 :: Music Pitch
-mel1 = let first = (a 3 en :+: cs 4 en :+: ds 4 en :+: fs 4 en) :=: a 2 hn 
-           second = (af 3 en :+: cs 4 en :+: ds 4 en :+: fs 4 en) :=: af 2 hn 
+mel1 = let first = (a 4 en :+: cs 4 en :+: ds 4 en :+: fs 4 en) :=: a 3 hn 
+           second = (af 4 en :+: cs 4 en :+: ds 4 en :+: fs 4 en) :=: af 3 hn 
        in first :+: first :+: first :+: first
           :+: second :+: second :+: second :+: second
 
 mel2 :: Music Pitch
-mel2 = let first = (fs 3 en :+: af 4 en :+: a 4 en :+: cs 4 en) :=: fs 2 hn
-           second = (g 3 en :+: b 4 en :+: cs 4 en :+: d 4 en) :=: g 2 hn
-           third = (g 3 en :+: a 4 en :+: cs 4 en :+: d 4 en) :=: g 2 hn
+mel2 = let first = (fs 4 en :+: af 4 en :+: a 4 en :+: cs 4 en) :=: fs 3 hn
+           second = (g 4 en :+: b 4 en :+: cs 4 en :+: d 4 en) :=: g 3 hn
+           third = (g 4 en :+: a 4 en :+: cs 4 en :+: d 4 en) :=: g 3 hn
        in first :+: first :+: first :+: first
           :+: second :+: third :+: second :+: third
 
