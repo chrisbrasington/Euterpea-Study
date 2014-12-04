27fefd071676f16c2712092cfe008f88332bca95
chrisbrasington <chrisbrasington@gmail.com>
2014-12-03 18:33:58 -0700

    Ludovico Einaudi inspired

---
+++ ludovico.hs
@@ -0,0 +1,15 @@
+import Euterpea
+
+mel1 :: Music Pitch
+mel1 = let 
+           chord1 = (a 3 wn :=: a 5 wn :=: e 4 wn )
+           chord2 = (c 4 wn :=: c 5 wn :=: g 4 wn)
+           chord3 = (g 3 wn :=: g 5 wn :=: d 4 wn )
+           chord3b = (g 3 hn :=: g 5 hn :=: d 4 hn )
+           chord4 = (gs 3 hn :=: gs 5 hn :=: ds 4 hn )
+       in chord1 :+: chord2 :+: chord3 :+: chord3b :+: chord4
+
+main = play 
+  (
+    mel1 :+: mel1
+  )
